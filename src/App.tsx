e90ce8a69404721f8c528a94c8b939fcc852ee20
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-08-13 03:50:48 -0400

    fix: minor styling fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ const App = () => {
   return (
     <div className="bg-gray-300 min-h-screen">
       <div className="bg-gray-100 flex">
-        <div className={`${boxWidth} lg:w-1/2 h-[50vh] py-10 px-8 lg:py:15 lg:px-12`}>
-          <div className="xl:max-w-2xl xl:ml-auto h-full">
-            <div className="flex flex-col gap-y-4 h-full justify-center">
+        <div className={`${boxWidth} lg:w-1/2 h-[50vh] py-10 px-5 lg:py:15 lg:px-12 border`}>
+          <div className="xl:max-w-2xl xl:ml-auto h-full border">
+            <div className="flex flex-col gap-y-2 sm:gap-y-4 h-full justify-center">
               <h1
-                className="w-full text-4xl xl:text-6xl font-semibold text-gray-800 leading-tight text-balance text-start"
+                className="w-full text-3xl sm:text-5xl xl:text-6xl font-semibold text-gray-800 leading-tight text-balance text-start"
               >
             If you can imagine it,
                 <br className="inline" />
                 <span className="text-indigo-500">You can make it real.</span>
               </h1>
-              <p className="text-gray-600 sm:text-2xl text-balance text-start">
+              <p className="text-gray-600 text-md sm:text-2xl text-balance text-start">
             My goal is to bring your ideas and ambitions into concrete projects that solve problems in a creative and stilized way.
               </p>
             </div>
