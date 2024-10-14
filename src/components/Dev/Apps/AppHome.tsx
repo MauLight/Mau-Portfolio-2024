98c30e63d83b1f4b8c40a8b10a9875daf320796b
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-10-14 19:17:39 -0300

    feat: remove unussed components

--- src/components/Dev/Apps/AppHome.tsx
+++ src/components/Dev/Apps/AppHome.tsx
@@ -6,15 +6,15 @@ import { EcommerceHome } from './Ecommerce/EcommerceHome'
 const AppHome = (): ReactElement => {
   const [authOneDisplay, setAuthOneDisplay] = useState({ preview: true, code: false })
   const [authTwoDisplay, setAuthTwoDisplay] = useState({ preview: true, code: false })
-  const [authThreeDisplay, setAuthThreeDisplay] = useState({ preview: true, code: false })
-  const [authFourDisplay, setAuthFourDisplay] = useState({ preview: true, code: false })
+  // const [authThreeDisplay, setAuthThreeDisplay] = useState({ preview: true, code: false })
+  // const [authFourDisplay, setAuthFourDisplay] = useState({ preview: true, code: false })
 
   return (
     <div className='w-full min-h-screen flex flex-col justify-center items-center gap-y-20 pt-32 pb-20 px-10'>
       <div className="w-full flex flex-col gap-y-1">
         <div className="flex gap-x-2 justify-end px-5">
           <button onClick={() => { setAuthOneDisplay({ preview: true, code: false })}} className={`text-[14px] font-light ${authOneDisplay.preview ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Preview</button>
-          <button onClick={() => { setAuthOneDisplay({ preview: false, code: true })}} className={`text-[14px] font-light ${authOneDisplay.code ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Code</button>
+          {/* <button onClick={() => { setAuthOneDisplay({ preview: false, code: true })}} className={`text-[14px] font-light ${authOneDisplay.code ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Code</button> */}
         </div>
         <div className="w-full h-[700px] border rounded-[20px] overflow-hidden">
           {
@@ -28,7 +28,7 @@ const AppHome = (): ReactElement => {
       <div className="w-full flex flex-col gap-y-1">
         <div className="flex gap-x-2 justify-end px-5">
           <button onClick={() => { setAuthTwoDisplay({ preview: true, code: false })}} className={`text-[14px] font-light ${authTwoDisplay.preview ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Preview</button>
-          <button onClick={() => { setAuthTwoDisplay({ preview: false, code: true })}} className={`text-[14px] font-light ${authTwoDisplay.code ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Code</button>
+          {/* <button onClick={() => { setAuthTwoDisplay({ preview: false, code: true })}} className={`text-[14px] font-light ${authTwoDisplay.code ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Code</button> */}
         </div>
         <div className="w-full h-[700px] border rounded-[20px] overflow-hidden">
           {
@@ -39,7 +39,7 @@ const AppHome = (): ReactElement => {
           }
         </div>
       </div>
-      <div className="w-full flex flex-col gap-y-1">
+      {/* <div className="w-full flex flex-col gap-y-1">
         <div className="flex gap-x-2 justify-end px-5">
           <button onClick={() => { setAuthThreeDisplay({ preview: true, code: false })}} className={`text-[14px] font-light ${authThreeDisplay.preview ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Preview</button>
           <button onClick={() => { setAuthThreeDisplay({ preview: false, code: true })}} className={`text-[14px] font-light ${authThreeDisplay.code ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Code</button>
@@ -52,8 +52,8 @@ const AppHome = (): ReactElement => {
             authThreeDisplay.code ? <div>hey</div> : null
           }
         </div>
-      </div>
-      <div className="w-full flex flex-col gap-y-1">
+      </div> */}
+      {/* <div className="w-full flex flex-col gap-y-1">
         <div className="flex gap-x-2 justify-end px-5">
           <button onClick={() => { setAuthFourDisplay({ preview: true, code: false })}} className={`text-[14px] font-light ${authFourDisplay.preview ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Preview</button>
           <button onClick={() => { setAuthFourDisplay({ preview: false, code: true })}} className={`text-[14px] font-light ${authFourDisplay.code ? 'text-indigo-500' : 'text-gray-600'} py-2`}>Code</button>
@@ -66,7 +66,7 @@ const AppHome = (): ReactElement => {
             authFourDisplay.code ? <div>hey</div> : null
           }
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
