92a4165c2a3a040eb396d74f7a66f0870c7e8289
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-02-10 09:56:28 -0300

    feat: add new loader
    acts on first visit

--- src/components/Common/Loader.tsx
+++ src/components/Common/Loader.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react'
-import { BarLoader } from 'react-spinners'
-import { TitleText } from './TitleText'
+import { WhiteAgent as Agent } from '@/utils/classes'
+import { randRange } from '@/utils/functions'
+import { AnimatePresence, motion } from 'framer-motion'
+import video from '@/assets/video/loading.webm'
+import videoEye from '@/assets/video/loading2.webm'
+
 
 export const Loader = () => {
   const [visible, setVisible] = useState(true)
@@ -8,17 +12,95 @@ export const Loader = () => {
   useEffect(() => {
     setTimeout(() => {
       setVisible(false)
-    }, 2000)
+    }, 3000)
   }, [])
+
+
+  const [width] = useState(2000)
+  const [height] = useState(1400)
+  const agents: Agent[] = []
+
+  useEffect(() => {
+    const canvas = document.getElementById('loader') as HTMLCanvasElement
+    if (canvas) {
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+
+      for (let i = 0; i < 400; i++) {
+        const x = randRange(0, width)
+        const y = randRange(0, height)
+        agents.push(new Agent(x, y))
+      }
+
+      const render = () => {
+        ctx.clearRect(0, 0, width, height)
+
+        for (let i = 0; i < agents.length; i++) {
+          const agent = agents[i]
+
+          for (let j = i + 1; j < agents.length; j++) {
+            const other = agents[j]
+            const dist = agent.pos.getDistance(other.pos)
+            if (dist > 200) continue
+
+            ctx.lineWidth = 1 - dist / 200
+            ctx.beginPath()
+            ctx.moveTo(agent.pos.x, agent.pos.y)
+            ctx.lineTo(other.pos.x, other.pos.y)
+            ctx.strokeStyle = '#D1D5DB'
+            ctx.stroke()
+          }
+        }
+
+        agents.forEach(agent => {
+          agent.update()
+          agent.draw(ctx)
+          agent.bounce(width, height)
+        })
+
+        // const x = randRange(0, width)
+        // const y = randRange(0, height)
+        // agents.push(new Agent(x, y))
+
+        agents.pop()
+      }
+
+      const loop = () => {
+        render()
+        requestAnimationFrame(loop)
+      }
+      loop()
+      return () => ctx.clearRect(0, 0, width, height)
+    }
+  })
+
+
 
   return (
     <>
       {
         visible && (
-          <div className={`absolute h-screen w-screen left-0 top-0 overflow-hidden flex flex-col justify-center items-center gap-y-2 ${!visible ? 'bg-transparent' : 'bg-[#fdefff]'} z-50 transition-all duration-200`}>
-            <TitleText size='text-2xl' text="Loading" />
-            <BarLoader color='#10100e' />
-          </div>
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 2.5, ease: 'easeOut' }}
+              className={`absolute h-screen w-screen left-0 top-0 overflow-hidden flex flex-col justify-center items-center gap-y-2 ${!visible ? 'bg-transparent' : ''} z-50 transition-all duration-200`}>
+              <div className="absolute z-10 top-0 right-0 p-10 h-full max-[1440px]:w-[500px] flex flex-col justify-center items-end">
+                <canvas className='' width={width} height={height} id='loader' />
+              </div>
+              <div className='absolute z-0 bg-[#10100e] w-screen h-screen opacity-70'></div>
+              <motion.video
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
+                className='absolute w-screen h-screen object-cover' src={video} autoPlay loop muted />
+              <motion.video
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{ duration: 0.8 }}
+                className='w-screen h-screen object-cover' src={videoEye} autoPlay loop muted />
+            </motion.div>
+          </AnimatePresence>
         )
       }
     </>
--- src/utils/classes.ts
+++ src/utils/classes.ts
@@ -47,3 +47,38 @@ export class Agent {
     ctx.restore()
   }
 }
+
+export class WhiteAgent {
+  pos: Vector
+  vel: Vector
+  radius: number
+  x: number
+  y: number
+  constructor(x: number, y: number) {
+    this.pos = new Vector(x, y)
+    this.vel = new Vector(randRange(-1, 0), randRange(-1, 0))
+    this.radius = randRange(2, 13)
+    this.x = x
+    this.y = y
+  }
+
+  bounce(width: number, height: number) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1
+  }
+
+  update() {
+    this.pos.x += this.vel.x
+    this.pos.y += this.vel.y
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.save()
+    ctx.translate(this.pos.x, this.pos.y)
+    ctx.beginPath()
+    ctx.fillStyle = '#fff'
+    ctx.arc(0, 0, this.radius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.restore()
+  }
+}
