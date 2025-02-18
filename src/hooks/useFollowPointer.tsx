0199915daaffdd1c214883f367c9c61d25a4dc41
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-02-18 01:49:15 -0300

    feat: stop usePointer to run if pathname is not home

--- src/hooks/useFollowPointer.tsx
+++ src/hooks/useFollowPointer.tsx
@@ -1,5 +1,6 @@
 import { RefObject, useEffect } from 'react'
 import { useMotionValue, useSpring, frame } from 'framer-motion'
+import { useLocation } from 'react-router'
 
 const spring = { damping: 10, stiffness: 100, restDelta: 0.001 }
 
@@ -9,8 +10,10 @@ export const useFollowPointer = (ref: RefObject<HTMLElement>) => {
   const x = useSpring(xPoint, spring)
   const y = useSpring(yPoint, spring)
 
+  const { pathname } = useLocation()
+
   useEffect(() => {
-    if (!ref.current) return
+    if (!ref.current || pathname.length === 1) return
     const handlePointerMove = ({ clientX, clientY }: MouseEvent) => {
       const element = ref.current!
       frame.read(() => {
